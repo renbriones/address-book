5e6d29325344c3ad3a7f0c8c79d8dd872aa54923
renbriones <irene.t.briones@gmail.com>
2014-05-28 07:08:34 +0100

    read from file

--- src/main/java/com/exercise/AddressBook.java
+++ src/main/java/com/exercise/AddressBook.java
@@ -1,7 +1,15 @@
 package com.exercise;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,6 +22,11 @@ public class AddressBook
 
     private List<Person> persons;
 
+    public AddressBook()
+    {
+
+    }
+
     public AddressBook(List<Person> persons)
     {
         this.persons = persons;
@@ -25,9 +38,57 @@ public class AddressBook
     }
 
     /**
+     * Read AddressBook file and parse content
+     * 
+     * @throws Exception
+     */
+    public void initAddressBook(String filename) throws Exception
+    {
+        try
+        {
+
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+            InputStream in = classloader.getResourceAsStream(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            this.persons = new ArrayList<Person>();
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                String[] personStr = line.split(",");
+                if (personStr.length < 3)
+                {
+                    // missing data
+                    // LOG warning
+                    continue;
+                }
+
+                try
+                {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+                    Date dob = dateFormat.parse(personStr[2]);
+                    persons.add(new Person(personStr[0].trim(), personStr[1].trim(), dob));
+                }
+                catch (ParseException pe)
+                {
+                    // LOG warning
+                    continue;
+                }
+            }
+        }
+        catch (IOException ioe)
+        {
+            // LOG error
+            throw ioe;
+        }
+    }
+
+    /**
+     * Find person with passed name
      * 
      * @param name
-     * @return
+     * @return the Person
      */
     public Person find(String name)
     {
@@ -47,13 +108,27 @@ public class AddressBook
         return null;
     }
 
+    /**
+     * Filter List
+     * 
+     * @param filter
+     * @return the filtered list
+     */
     public List<Person> filter(Filter filter)
     {
         return filter.filterList(persons);
     }
 
+    /**
+     * Sorts List of Person
+     * 
+     * @param comparator
+     */
     public void sort(Comparator<Person> comparator)
     {
-        Collections.sort(persons, new PersonDOBComparator());
+        if (persons != null)
+        {
+            Collections.sort(persons, new PersonDOBComparator());
+        }
     }
 }
--- src/test/java/com/exercise/AddressBookTest.java
+++ src/test/java/com/exercise/AddressBookTest.java
@@ -1,6 +1,7 @@
 package com.exercise;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
@@ -66,7 +67,6 @@ public class AddressBookTest
     @Test
     public void testFindWhenMoreThanTwoNames()
     {
-
         List<Person> persons = new ArrayList<Person>();
         persons.add(new Person("first person", "male", new Date()));
         persons.add(new Person("first person", "female", new Date()));
@@ -76,5 +76,17 @@ public class AddressBookTest
         Person person = aBook.find("first");
         assertEquals("Found the person", "first person", person.getName());
         assertEquals("Found the person", "male", person.getGender());
+    }
+
+    @Test
+    public void testInitAddressBook() throws Exception
+    {
+        AddressBook aBook = new AddressBook();
+        aBook.initAddressBook("AddressBookTest");
+
+        assertNotNull(aBook.getPersons());
+        assertEquals("Found the person", "Gemma Lane", aBook.find("Gemma").getName());
+        assertNull("Error in date, Will not include date Paul", aBook.find("Paul"));
+        assertNull("With missing data, Will not include date Sarah", aBook.find("Sarah"));
     }
 }
